33b7f7cbed957e4af3db6bbe0065d2d9c3c446dd
Youness Sahraoui <ysahraou@c4r2p7.1337.ma>
2023-12-13 17:06:01 +0100

    some edits for %s

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: ysahraou <ysahraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:50:28 by ysahraou          #+#    #+#             */
-/*   Updated: 2023/12/13 13:36:45 by ysahraou         ###   ########.fr       */
+/*   Updated: 2023/12/13 16:55:20 by ysahraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int	ft_putstr(char *s)
 	int	count;
 
 	count = 0;
+	if (s == NULL)
+	{
+		count += write(1, "(null)", 6);
+		return (count);
+	}
 	while (*s)
 	{
 		write(1, s, 1);
