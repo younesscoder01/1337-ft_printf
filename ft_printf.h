80d2588ca4befa3d82af95f94574c4a6aa21f309
Youness Sahraoui <ysahraou@c4r1p9.1337.ma>
2023-12-19 12:04:40 +0100

    norm

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ysahraou <ysahraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:43:53 by ysahraou          #+#    #+#             */
-/*   Updated: 2023/12/19 10:39:52 by ysahraou         ###   ########.fr       */
+/*   Updated: 2023/12/19 12:04:13 by ysahraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <stdarg.h>
 
 int		ft_putchar(char c);
-int	ft_printf(const char *type, ...);
+int		ft_printf(const char *type, ...);
 int		ft_putstr(char *s);
 void	ft_putnbr_u(unsigned int n, int *count);
 void	ft_printhex(unsigned int n, int *count, char c);
